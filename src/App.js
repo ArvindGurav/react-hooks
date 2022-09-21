a21b198e32995bffa5ccb95ff0b6bc5fc9bc5e87
ArvindGurav <31888466+ArvindGurav@users.noreply.github.com>
2022-09-22 00:31:43 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import UseEffectEg from "./components/UseEffectEg";
 import UseEffecteg2 from "./components/UseEffecteg2";
 import UseMemoEg from "./components/UseMemoEg";
+import UseRefEg from "./components/UseRefEg";
 import UseStateEg from "./components/UseStateEg";
 
 function App() {
 
   return (
     <>
-      <UseMemoEg/>
+      <UseRefEg/>
     </>
   );
 }
--- src/components/UseRefEg.js
+++ src/components/UseRefEg.js
@@ -1,14 +1,24 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function UseRefEg() {
 
     const [name, setName] = useState('')
     const inputRef = useRef()
+    const prevName = useRef('')
+
+    function focusInputRef() {
+        inputRef.current.focus()
+    }
+
+    useEffect( () => {
+        ///if we use state instead of ref than it will go in infinte loop
+        prevName.current = name
+    }, [name])
 
   return (
     <>
         <input ref={inputRef} value={name} onChange={ (e) => setName(e.target.value)}></input>
-        <div>My name is {name}</div>
+        <div>My name is {name} and it used to be {prevName.current}</div>
         <button onClick={focusInputRef}>Focus</button>
     </>
   )
