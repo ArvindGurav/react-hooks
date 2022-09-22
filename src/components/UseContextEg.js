a6df14a940c3c5cf6eae1dfbe8bd01984c7443ad
ArvindGurav <31888466+ArvindGurav@users.noreply.github.com>
2022-09-22 22:42:38 +0530

    Create UseContextEg.js

---
+++ src/components/UseContextEg.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const ThemeContext = React.createContext()
+
+function UseContextEg() {
+
+    const [darkTheme, setDarkTheme] = useState(true)
+
+    function toggleTheme() {
+        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    }
+
+  return (
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <FunctionContextComponent/>
+        <ClassContextComponent/>
+      </ThemeContext.Provider>
+    </>
+  )
+}
+
+export default UseContextEg
